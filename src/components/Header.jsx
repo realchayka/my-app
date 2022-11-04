3c19120be172e0cb30dc21eef3f61f957fc18d07
Roman Maximov <lonelyplay54@gmail.com>
2022-11-04 15:07:46 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://img.freepik.com/free-vector/logotype-set_53876-120502.jpg?w=2000"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+    <div>
+      <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1"></img>
+    </div>
+    <div>
+      ava + descripion
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Profile;
